7b4686f7c5282e80f4dc3ee4796822f167520573
Erik Medina <erikmedina@iMac-de-Erik.local>
2016-05-24 18:18:14 -0500

    se agrega el FRM seleccion de color

--- app/src/main/java/pe/edu/upc/tuneamilook/MainActivity.java
+++ app/src/main/java/pe/edu/upc/tuneamilook/MainActivity.java
@@ -1,7 +1,11 @@
 package pe.edu.upc.tuneamilook;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import layout.seleccionColor.seleccionColor;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void AbrirFRMobtenerColores(View view)
+    {
+        Intent intent = new Intent(MainActivity.this, seleccionColor.class);
+        startActivity(intent);
     }
 }
